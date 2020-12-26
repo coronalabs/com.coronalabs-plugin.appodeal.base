8c9154145b5af3635f43742eef502644382c8abd
Vlad Shcherban <vlad@coronalabs.com>
2020-12-26 20:55:47 +0200

    Updating to 2.8.1
    Updating to 2.8.1

--- plugins/2020.3569/iphone/metadata.lua
+++ plugins/2020.3569/iphone/metadata.lua
@@ -3,9 +3,9 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'plugin_appodeal_base', "xml2", "z", 'APDAppodealAdExchangeAdapter' },
-		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", },
-		frameworksOptional = {"SafariServices", "AdSupport"},
+		staticLibs = { 'plugin_appodeal_base', "xml2", "z", },
+		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "StackFoundation", "StackMRAIDKit", "StackNASTKit", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "Appodeal", "Protobuf"},
+		frameworksOptional = {"SafariServices", "AdSupport", "AppTrackingTransparency"},
 		usesSwift  = true,
 	},
 }
