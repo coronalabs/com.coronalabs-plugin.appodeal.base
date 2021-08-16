847dde12af7744f7e7ba2d2b7f30137e9ae902ca
Vlad Shcherban <vlad@coronalabs.com>
2021-08-16 20:12:46 +0300

    Updating to 2.10.1

--- plugins/2018.3326/android/metadata.lua
+++ plugins/2018.3326/android/metadata.lua
@@ -5,8 +5,6 @@ local metadata =
 		format = "jar",
 		manifest =
 		{
-			permissions = {},
-
 			usesPermissions =
 			{
 				"android.permission.INTERNET",
@@ -19,58 +17,14 @@ local metadata =
 				{ name="android.hardware.location.network", required=false },
 				{ name="android.hardware.location.gps", required=false }
 			},
-
-			applicationChildElements =
-			{
-        [[
-				<meta-data android:name="com.appodeal.framework" android:value="corona" />
-        ]],
-        [[
-        <activity android:name="com.appodeal.ads.InterstitialActivity"
-            android:configChanges="orientation|screenSize"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen" />
-        <activity android:name="com.appodeal.ads.VideoActivity"
-            android:configChanges="orientation|screenSize"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen" />
-        <activity android:name="com.appodealx.mraid.MraidActivity"
-            android:configChanges="orientation|screenSize"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen"/>
-        <activity android:name="com.appodeal.ads.VideoPlayerActivity"
-            android:theme="@android:style/Theme.Black.NoTitleBar.Fullscreen"/>
-        <receiver android:name="com.appodeal.ads.AppodealPackageAddedReceiver" android:exported="true" android:enabled="true">
-            <intent-filter>
-                <action android:name="android.intent.action.PACKAGE_ADDED" />
-                <data android:scheme="package" />
-            </intent-filter>
-        </receiver>
-        <activity android:name="com.appodeal.ads.TestActivity"
-            android:theme="@android:style/Theme.Translucent.NoTitleBar" />
-
-        <meta-data android:name="com.google.android.gms.version" android:value="@integer/google_play_services_version" />
-        <activity android:name="com.google.android.gms.ads.AdActivity"
-            android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize"
-            android:theme="@android:style/Theme.Translucent" />
-
-        <activity android:name="com.appodeal.iab.vast.activity.VastActivity"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen"
-            android:configChanges="keyboardHidden|orientation|screenSize"/>
-
-        <activity android:name="com.appodeal.ads.networks.vpaid.VPAIDActivity"
-            android:theme="@android:style/Theme.NoTitleBar.Fullscreen" />
-				]]
-			}
-		}
+		},
 	},
 
 	coronaManifest = {
-		dependencies = {
-			["shared.android.support.v4"] = "com.coronalabs",
-			["shared.android.support.v7.appcompat"] = "com.coronalabs",
-			["shared.android.support.v7.recyclerview"] = "com.coronalabs",
-			["shared.google.play.services.ads.identifier"] = "com.coronalabs",
-            ["shared.google.play.services.location"] = "com.coronalabs",
-            ["shared.google.play.services.gcm"] = "com.coronalabs"
-		}
+		-- dependencies = {
+		-- 	["shared.android.support.v4"] = "com.coronalabs",
+		-- 	["shared.android.support.v7.appcompat"] = "com.coronalabs",
+		-- }
 	}
 }
 
--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,9 +3,10 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'plugin_appodeal_base', "xml2", "z", 'APDAppodealAdExchangeAdapter', },
-		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", 'AppodealAdExchangeSDK', },
-		frameworksOptional = {"SafariServices", "AdSupport"},
+		staticLibs = { 'plugin_appodeal_base', "xml2", "z", },
+		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "StackFoundation", "StackMRAIDKit", "StackNASTKit", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "Appodeal", "Protobuf", "StackCrashReporter"},
+		frameworksOptional = {"SafariServices", "AdSupport", "AppTrackingTransparency"},
+		usesSwift  = true,
 	},
 }
 
