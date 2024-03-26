1ccb70d1ec20b6709b22cb9f21cbb3fc9c7c4650
Scott Harrison <scottrules44@gmail.com>
2024-03-26 12:27:45 -0500

    Bug Fixes

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'plugin_appodeal_base', "xml2", "z", "AppVerificationLibrary" },
-		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", "AppodealMediationCore", "OMSDK_Appodeal", "Protobuf", "StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML"},
+		staticLibs = { 'plugin_appodeal_base', "xml2", "z", "AppVerificationLibrary", "APDIABAdapter" },
+		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "WatchConnectivity", "SafariServices", "EventKit", "Appodeal", "AppodealMediationCore", "OMSDK_Appodeal", "Protobuf", "StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "UserMessagingPlatform"},
 		frameworksOptional = {"SafariServices", "AdSupport", "AppTrackingTransparency"},
 		usesSwift  = true,
 	},
